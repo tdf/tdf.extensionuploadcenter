bfb13bdc464012b8deb9c1bdce95031522446446
andreasma <maand@gmx.de>
2015-12-21 21:15:58 +0100

    Removed the notification about a new version for the moment, because it causes some issues with the sharing feature.

--- tdf/extensionuploadcenter/eupcenter.py
+++ tdf/extensionuploadcenter/eupcenter.py
@@ -146,26 +146,6 @@ def notifyAboutNewProject(eupproject, event):
     )
 
 
-def notifiyAboutNewVersion(eupproject, event):
-    if hasattr(event, 'descriptions') and event.descriptions:
-        for d in event.descriptions:
-            if d.interface is IEUpCenter and 'available_versions' in d.attributes:
-                users=api.user.get_users()
-                message='We added a new version of LibreOffice to the list.\n' \
-                        'Please add this version to your LibreOffice extension release(s), ' \
-                        'if it is (they are) compatible with this version.\n\n' \
-                        'Kind regards,\n\n' \
-                        'The LibreOffice Extension and Template Site Administration Team'
-                for f in users:
-                    mailaddress = f.getProperty('email')
-                    api.portal.send_email(
-                        recipient=mailaddress,
-                        sender="noreply@libreoffice.org",
-                        subject="New Version of LibreOffice Added",
-                        body=message,
-                    )
-
-
 
 # Views
 
