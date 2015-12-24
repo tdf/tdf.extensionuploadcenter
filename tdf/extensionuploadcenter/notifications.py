2136c1c8f8bc0f0fd7033757771357c696b9213c
andreasma <maand@gmx.de>
2015-12-24 09:28:09 +0100

    Adding a notifications modul.

---
+++ tdf/extensionuploadcenter/notifications.py
@@ -0,0 +1,23 @@
+from plone import api
+from tdf.extensionuploadcenter.eupcenter import IEUpCenter
+
+
+
+def notifiyAboutNewVersion(eupproject, event):
+   if hasattr(event, 'descriptions') and event.descriptions:
+       for d in event.descriptions:
+           if hasattr(d, 'interface') and d.interface is IEUpCenter and 'available_versions' in d.attributes:
+               users=api.user.get_users()
+               message='We added a new version of LibreOffice to the list.\n' \
+                       'Please add this version to your LibreOffice extension release(s), ' \
+                       'if it is (they are) compatible with this version.\n\n' \
+                       'Kind regards,\n\n' \
+                       'The LibreOffice Extension and Template Site Administration Team'
+               for f in users:
+                   mailaddress = f.getProperty('email')
+                   api.portal.send_email(
+                       recipient=mailaddress,
+                       sender="noreply@libreoffice.org",
+                       subject="New Version of LibreOffice Added",
+                       body=message,
+                   )
