2cac4cd07f58f4b61c3890a84e7c7fbf4b927744
andreasma <maand@gmx.de>
2020-03-30 15:19:32 +0200

    Back to development: 0.50

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = '\n\n'.join([
 
 
 setup(name='tdf.extensionuploadcenter',
-      version='0.49',
+      version='0.50.dev0',
       description="TDF Extension Upload Center",
       long_description=long_description,
       # Get more strings from
